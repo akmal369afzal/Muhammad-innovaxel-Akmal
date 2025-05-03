adcc3dc97661c779a114af5a14508335bc64f7c7
akmal369afzal <akmal246afzal@gmail.com>
2025-05-03 11:49:49 +0500

    index file setup and mongodb connection established

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./utils/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); 
+
+app.listen(PORT, async () => {
+	await connectDB();
+    console.log("Server is running on http://localhost:" + PORT);
+});

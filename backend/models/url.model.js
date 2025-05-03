2662acbdf1ced05660d1bad0933238580a16a652
akmal369afzal <akmal246afzal@gmail.com>
2025-05-03 12:04:04 +0500

    URL schema created

---
+++ backend/models/url.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const urlSchema = new mongoose.Schema({
+	url: {
+		type: String,
+		required: true,
+	},
+	shortCode: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+    clickCount: {
+        type: Number,
+        default: 0
+    }
+}, { timestamps: true });
+
+
+const Url = mongoose.model("Url", urlSchema);
+
+export default Url;

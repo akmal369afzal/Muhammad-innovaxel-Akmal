d60b3367edd762e1f9aaa35cb6b966a1a3c9bfa3
akmal369afzal <akmal246afzal@gmail.com>
2025-05-03 12:45:25 +0500

    route for createShortUrl made

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./utils/db.js";
+import urlRoutes from "./routes/url.route.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); 
+app.use(express.json());
+
+app.use(express.static("frontend"));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
+});
+
+app.use("/shorten", urlRoutes);
 
 app.listen(PORT, async () => {
-	await connectDB();
+    await connectDB();
     console.log("Server is running on http://localhost:" + PORT);
 });
